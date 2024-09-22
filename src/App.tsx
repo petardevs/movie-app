2a4f366e4e359eaab81155a0d76d97f8e0c9f6cb
petarpla <124912810+petardevs@users.noreply.github.com>
2024-09-22 20:30:05 +0200

    Implement dark mode and brand update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-
+import { Grid, GridItem, Show, Flex, Box } from "@chakra-ui/react";
+import Navbar from "./components/UI/Navbar";
 
 function App() {
-  
-
   return (
     <>
-     
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" paddingX={5}></GridItem>
+        </Show>
+        <GridItem area="main">
+          <Box paddingLeft={2}>
+            <Flex gap={5} marginBottom={5}></Flex>
+          </Box>
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
