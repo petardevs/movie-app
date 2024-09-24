49b7d61ea3ec31cff6096a78fa37bdd56fb7c3a6
petarpla <124912810+petardevs@users.noreply.github.com>
2024-09-24 23:08:50 +0200

    Implement image

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,8 +1,10 @@
-import { Card, CardBody, HStack, Heading } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import image from "../assets/img/pb.webp"
 
 const MovieCard = () => {
   return (
-    <Card>
+    <Card padding={3}>
+      <Image src={image} borderRadius={10}></Image>
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}></HStack>
         <Heading fontSize="2xl">Prison Break</Heading>
