349d7fe7207819ced83f1d0b01f3e9fd40b4bd2c
petarpla <124912810+petardevs@users.noreply.github.com>
2024-09-24 22:55:17 +0200

    Format code

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,21 +1,14 @@
-import { Card, CardBody, HStack, Heading } from "@chakra-ui/react"
-
+import { Card, CardBody, HStack, Heading } from "@chakra-ui/react";
 
 const MovieCard = () => {
   return (
-   <Card>
-      
-   <CardBody>
-     <HStack justifyContent="space-between" marginBottom={3}>
-       
-       
-     </HStack>
-     <Heading fontSize="2xl">
-      Prison Break
-     </Heading>
-   </CardBody>
- </Card>
-  )
-}
+    <Card>
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}></HStack>
+        <Heading fontSize="2xl">Prison Break</Heading>
+      </CardBody>
+    </Card>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
