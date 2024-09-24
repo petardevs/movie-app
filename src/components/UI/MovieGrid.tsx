0bf67509813a31ea254845f358770ffe258cc3aa
petarpla <124912810+petardevs@users.noreply.github.com>
2024-09-24 23:34:56 +0200

    Refactor

--- src/components/UI/MovieGrid.tsx
+++ src/components/UI/MovieGrid.tsx
@@ -7,7 +7,6 @@ const MovieGrid = () => {
       columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
       padding="10px"
       spacing={6}
-      overflow={"scroll"}
     >
       <MovieCard></MovieCard>
       <MovieCard></MovieCard>
